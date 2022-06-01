08544ec69ed6ba27bdb6a208e8fe6ac95c7e0963
bowen <bowenivan16@gmail.com>
2022-06-01 12:46:51 +0300

    the code which we write in a specific method would be launched up as soon as applicationContext is ready

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,13 +1,28 @@
 package com.in28minutes.database.databasedemo;
 
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// the code which we write in a specific method would be launched up as soon as
+// applicationContext is ready
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	PersonJdbcDAO dao;
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// fire the query
+		logger.info("All users -> {}", dao.findAll());
+	}
 }
