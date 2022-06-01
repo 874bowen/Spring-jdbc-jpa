f5c08595d8d2909b06cfdadf8cfb2cbe43a9c59c
bowen <bowenivan16@gmail.com>
2022-06-01 12:45:32 +0300

    to insert values at runtime to the person table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,10 @@ create table person
     birth_date timestamp,
     primary key(id)
 );
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) VALUES(10001, 'Francis', 'Thika', now());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) VALUES(10002, 'Ivan', 'Eldoret', now());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) VALUES(10003, 'Bowen', 'Nakuru', now());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) VALUES(10004, 'Eileen', 'Kakamega', now());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) VALUES(10005, 'Daisy', 'Nairobi', now());
+
+select * from person;
