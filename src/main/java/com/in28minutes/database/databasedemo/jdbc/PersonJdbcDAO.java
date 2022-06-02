82c83f9c633c2a97714d9de6f5d1f4a11df8e215
bowen <bowenivan16@gmail.com>
2022-06-02 08:05:01 +0300

    using BeanPropertyRowMapper

--- src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -4,8 +4,12 @@ import com.in28minutes.database.databasedemo.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -32,8 +36,8 @@ public class PersonJdbcDAO {
     //select * from person - from a query we get a result set
     public List<Person> findAll(){
         // in SpringJdbc there a automatic mapper so when column name matches - id -> id in the db
-        return jdbcTemplate.query("select * from person",
-                new BeanPropertyRowMapper<Person>(Person.class));
+//        return jdbcTemplate.query("select * from person",
+//                new BeanPropertyRowMapper<Person>(Person.class));
         // Whenever we are using beanROwMapper the Person bean should hava an empty constructor
         // Simple no args constructor
     }
